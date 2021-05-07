0429759e10fe7d8e9ff79409fc912fd21693e933
Mohamed Emad <bon.dok2000@hotmail.com>
2021-05-07 21:48:08 +0200

    allow headers

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers: Content-Type");
   next();
 });
 
